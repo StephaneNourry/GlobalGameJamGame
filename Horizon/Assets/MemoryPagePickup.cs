0c15874b014b7b3a87b1d2693544b68601de0c84
unknown <stephane.nourry@epitech.eu>
2021-01-31 16:53:08 +0400

    Game first part done need some improvement

---
+++ Horizon/Assets/MemoryPagePickup.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryPagePickup : MonoBehaviour
+{
+    public GameObject UIObject;
+    public GameObject Hud;
+
+    Pickup m_Pickup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Pickup = GetComponent<Pickup>();
+        DebugUtility.HandleErrorIfNullGetComponent<Pickup, HealthPickup>(m_Pickup, this, gameObject);
+
+        // Subscribe to pickup action
+        m_Pickup.onPick += OnPicked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnPicked(PlayerCharacterController player)
+    {
+        Hud.SetActive(false);
+        UIObject.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        m_Pickup.PlayPickupFeedback();
+        Destroy(gameObject);
+    }
+}
